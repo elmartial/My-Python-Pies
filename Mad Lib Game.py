eb52d7718cccf700153b0f9c90ce89793d5c1500
El Martial <elmartial17@gmail.com>
2024-09-19 17:56:35 +0600

    Create Mad Lib Game.py

---
+++ Mad Lib Game.py
@@ -0,0 +1,8 @@
+#mad lib game
+color = input("What color is the rose?\n")
+plural_noun = input("Enter a plural noun:\n")
+celebrity = input("Who's your celebrity crush?\n")
+
+print("\nRoses are " + color)
+print(plural_noun + " are blue")
+print(celebrity + ", I love you!")
